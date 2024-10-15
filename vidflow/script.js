4e8238fa08d792394baa37d8ca2e8df76f032c14
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-15 13:38:30 -0300

    Tratando erros com trycatch

--- vidflow/script.js
+++ vidflow/script.js
@@ -1,11 +1,16 @@
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
-  const busca = await fetch("http://localhost:3000/videos");
-  const videos = await busca.json();
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
 
-  videos.forEach((video) => {
-    containerVideos.innerHTML += `
+    videos.forEach((video) => {
+      if (video.categoria == "") {
+        throw new Error("Vídeo não tem categoria");
+      }
+
+      containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
@@ -16,7 +21,10 @@ async function buscarEMostrarVideos() {
               
         </li>
         `;
-  });
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
+  }
 }
 
 buscarEMostrarVideos();
