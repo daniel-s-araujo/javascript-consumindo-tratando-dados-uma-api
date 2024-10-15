cb6ecddf12336fc0e8595651b00b14eeb05d1f64
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-15 13:33:01 -0300

    Async/Await

--- vidflow/script.js
+++ vidflow/script.js
@@ -1,10 +1,11 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-  .then((res) => res.json())
-  .then((videos) =>
-    videos.forEach((video) => {
-      containerVideos.innerHTML += `
+async function buscarEMostrarVideos() {
+  const busca = await fetch("http://localhost:3000/videos");
+  const videos = await busca.json();
+
+  videos.forEach((video) => {
+    containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title"${video.titulo}" frameborder="0" allowfullscreen></iframe>
                 <div class="descricao-video">
@@ -15,8 +16,7 @@ const api = fetch("http://localhost:3000/videos")
               
         </li>
         `;
-    })
-  )
-  .catch((error) => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`;
   });
+}
+
+buscarEMostrarVideos();
