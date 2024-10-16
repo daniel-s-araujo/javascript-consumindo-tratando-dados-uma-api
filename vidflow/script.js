9e1bc020e0ff9a11bac4fecbcfe0202def404b50
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-16 13:54:12 -0300

    Construindo a lógica de pesquisa

--- vidflow/script.js
+++ vidflow/script.js
@@ -36,7 +36,18 @@ barraDePesquisa.addEventListener("input", filtrarPesquisa);
 function filtrarPesquisa() {
   const videos = document.querySelectorAll(".videos__item");
 
-  if (barraDePesquisa.value != null) {
+  if (barraDePesquisa.value != "") {
+    for(let video of videos) {
+      let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+      if (!titulo.includes(valorFiltro)) {
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
   } else {
+    videos.style.display = "block";
   }
 }
